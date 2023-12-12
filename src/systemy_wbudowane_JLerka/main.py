2891922864670794818bd238445287f9ec70e200
Jakub Lerka <jakublerka01@gmail.com>
2023-12-12 21:33:24 +0100

    Bonus UI and prepare for Bluetooth

--- src/systemy_wbudowane_JLerka/main.py
+++ src/systemy_wbudowane_JLerka/main.py
@@ -4,26 +4,37 @@ from tkinter import messagebox
 import ttkbootstrap as ttk
 import bluetooth
 
-
+index=0
 nearbyDevices = bluetooth.discover_devices(lookup_names=True)
-label_list = []
+devicesDictionary={}
 
 #Control window
 mainWindow=ttk.Window(themename='darkly')
 mainWindow.title("Systemy Wbudowane robot app")
 mainWindow.geometry("600x400")
 
+def connectToDevice():
+    bluetooth.BluetoothSocket
+
+#Adres MAC JBL Tune100BT - F4:BC:DA:03:D4:CA
+
 def createBluetoothWindow():
-    bluetoothWindow=ttk.Window() #Okno polaczenia do Bluetooth'a
+    bluetoothWindow=ttk.Window(themename='darkly') #Okno polaczenia do Bluetooth'a
     bluetoothWindow.title("Połączenie Bluetooth")
     bluetoothWindow.geometry("500x250")
     #new_label = ttk.Label(master=bluetoothWindow)
     #new_label.pack(pady=5)
     #label_list.append(new_label)
-    for addr, name in nearbyDevices:
-        label = ttk.Label("{} - {}\n".format(addr, name))
-        label.pack(pady=5)
-        label_list.append(label)
+    for address, name in nearbyDevices:
+        devicesDictionary[index+1] = {"address":address, "name":name}
+        labelButtonFrame = ttk.Frame(master=bluetoothWindow) #Frame do przetrzymywanie labela i buttona w pojedynczym wierszu
+        labelButtonFrame.pack(side="top",pady=5)
+
+        deviceLabel = ttk.Label(master=labelButtonFrame, text="{} - {} - {}".format(address, name, index))
+        
+        connectButton=ttk.Button(master=labelButtonFrame,text="Connect",command=connectToDevice)
+        deviceLabel.pack(side='left',padx=5)
+        connectButton.pack(side='left')
 
 
 #def bluetoothButtonClick():
@@ -44,26 +55,28 @@ titleLabel.pack()
 
 #Bluetooth field
 bluetoothFrame=ttk.Frame(master=mainWindow)
+bluetoothStatus=ttk.Label(master=bluetoothFrame, text="Connected: ")
 bluetoothLabel=tk.Label(master=bluetoothFrame, text="Connect to Bluetooth: ")
 bluetoothButton =tk.Button(master=bluetoothFrame, text="Connect!", command=createBluetoothWindow)
 bluetoothFrame.pack(pady=10)
 bluetoothLabel.pack(side='left', padx=10)
-bluetoothButton.pack(side='left')
+bluetoothStatus.pack(side="right",pady=2)
+bluetoothButton.pack(side='left',padx=10)
 
 
 #Arrows field
 arrowsFrame=ttk.Frame(master=mainWindow)
-upArrow=tk.PhotoImage(file = "src\\resources\\up-arrow-icon.png")
-upArrow=upArrow.subsample(5, 5)
+upArrow=tk.PhotoImage(file = "src\\resources\\up-arrow-icon-white.png")
+#upArrow=upArrow.subsample(5, 5)
 
-rightArrow=tk.PhotoImage(file = "src\\resources\\right-arrow-icon.png")
-rightArrow=rightArrow.subsample(5, 5)
+rightArrow=tk.PhotoImage(file = "src\\resources\\right-arrow-icon-white.png")
+#rightArrow=rightArrow.subsample(5, 5)
 
-leftArrow=tk.PhotoImage(file = "src\\resources\\left-arrow-icon.png")
-leftArrow=leftArrow.subsample(5, 5)
+leftArrow=tk.PhotoImage(file = "src\\resources\\left-arrow-icon-white.png")
+#leftArrow=leftArrow.subsample(5, 5)
 
-downArrow=tk.PhotoImage(file = "src\\resources\\down-arrow-icon.png")
-downArrow=downArrow.subsample(5, 5)
+downArrow=tk.PhotoImage(file = "src\\resources\\down-arrow-icon-white.png")
+#downArrow=downArrow.subsample(5, 5)
 
 arrowsFrame.pack(pady=20)
 
